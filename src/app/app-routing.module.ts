a3c9e8c8c8908084693c57915d142532af02afaf
letizia obiglio <letizia.obiglio@gmail.com>
2019-11-04 09:56:24 +0100

    build: route / done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PokedexPageComponent } from "./pages/pokedex-page/pokedex-page.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: PokedexPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
