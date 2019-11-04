55a18f23886c442441b18264c1549c0fb14e2125
letizia obiglio <letizia.obiglio@gmail.com>
2019-11-04 10:06:17 +0100

    build: attributs added to pokedexpage

--- src/app/pages/pokedex-page/pokedex-page.component.ts
+++ src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Pokemon } from "./../../shared/pokemon";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pokedex-page',
-  templateUrl: './pokedex-page.component.html',
-  styleUrls: ['./pokedex-page.component.scss']
+  selector: "app-pokedex-page",
+  templateUrl: "./pokedex-page.component.html",
+  styleUrls: ["./pokedex-page.component.scss"]
 })
 export class PokedexPageComponent implements OnInit {
+  pokemons: Pokemon[];
+  pokemon: Pokemon;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
