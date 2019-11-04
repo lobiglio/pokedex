66cfa68e1b2e6c9f529ce294b3a6467402de325f
letizia obiglio <letizia.obiglio@gmail.com>
2019-11-04 09:50:53 +0100

    build: pokedexpage component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexPageComponent } from './pages/pokedex-page/pokedex-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexPageComponent
   ],
   imports: [
     BrowserModule,
